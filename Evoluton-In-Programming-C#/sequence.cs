d164193e657372600e74bf61514901b8a0c97aa9
DarinaVT <darito.timoteeva10@gmail.com>
2024-11-10 18:34:04 +0200

    Create sequence.cs

---
+++ Evoluton-In-Programming-C#/sequence.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+BigInteger startingNumber = 1;
+for (int i = 1; i < 100; i++)
+{
+startingNumber = i + BigInteger.Pow(2, i) - 2;
+Console.WriteLine(startingNumber);
+}
